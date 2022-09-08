0071e8a7b20d5dd9ad0211c380fbea69ea6747b0
ysh329@sina.com <ysh329@sina.com>
2022-09-08 22:26:55 +0800

    test

--- hello.py
+++ hello.py
@@ -8,9 +8,35 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/aipugt/<path:g_path>/<path:i_path>/<path:w_path>/<path:cur_path>")
-def aipugt_run(g_path, i_path, w_path, cur_path):
-    """Run aipugt"""
+@app.route("/aipugt")
+def hello_aipugt():
+    return "<p>Hello, AIPUGT!</p>"
+
+@app.route("/aipugt/<path:cmd_params>")
+def aipugt(cmd_params):
+    # NOTE: replace space blank with +
+    print(f"cmd_params:{cmd_params}")
+    cmd = cmd_params.split("+")
+    cmd = list(filter(lambda s: s=="", cmd))
+
+    def parser_cmd(cmd):
+        import argparse
+        parser = argparser.ArgumentParser()
+        # TODO:replace here
+        args = parser.parse_args(cmd)
+        return args
+
+    args = parser_cmd(cmd)
+    print(f"cmd:{cmd}")
+
+    return jsonify({"cmd_params": cmd_params, "cmd":cmd})
+
+
+
+
+#@app.route("/aipugt/<path:g_path>/<path:i_path>/<path:w_path>/<path:cur_path>")
+def aipugt_params(g_path, i_path, w_path, cur_path):
+    """Run aipugt with params"""
 
     graph = str(g_path).strip()
     inputs = str(i_path).strip()
@@ -34,3 +60,6 @@ def aipugt_run(g_path, i_path, w_path, cur_path):
     print(f"{cur_path}/{weights}.bin")
 
     return html
+
+if __name__ == "__main__":
+    app.run(debug=True)
